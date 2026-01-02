a4853ef77e17a46c47b8aa9567718d68f9a7c17f
JCesarAguilar <jcesar.a.15@gmail.com>
2026-01-01 23:30:32 -0500

    error en context corregido

--- front/vite-project/src/context/AuthProvider.jsx
+++ front/vite-project/src/context/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect, useCallback } from "react";
 import { AuthContext } from "./AuthContext";
-import { API_URL } from "../config/config";
+import { API_URL } from "../config";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
