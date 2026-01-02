1c5603a2a10e736801e940e0ba3bad923a562e89
JCesarAguilar <jcesar.a.15@gmail.com>
2026-01-01 23:35:07 -0500

    done

--- front/vite-project/src/context/AuthProvider.jsx
+++ front/vite-project/src/context/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect, useCallback } from "react";
 import { AuthContext } from "./AuthContext";
-import { API_URL } from "../config";
+import { API_URL } from "../config/api";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
